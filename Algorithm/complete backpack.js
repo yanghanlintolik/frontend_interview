b7cc041160e331f82696ad69dd9c69769f618caa
yanghanlintolik <1258597126@qq.com>
2022-05-23 11:19:10 +0800

    完全背包

--- Algorithm/complete backpack.js
+++ Algorithm/complete backpack.js
@@ -9,6 +9,7 @@ const dp = new Array(m+1).fill(0)
 // 4、遍历顺序
 for(let i=0; i<a.length; i++){
     // 唯一不同点在于从小到大遍历，可以放入多个物品
+    // j=a[i]即此次循环起码要能放下i物品，再进行比较，如果从0开始，会发生数组越界，产生NaN
     for(let j=a[i]; j<=m; j++){
         // 2、递推公式
         dp[j] = Math.max(dp[j],dp[j-a[i]]+v[i])
